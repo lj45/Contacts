56f22c76f312c5875aa17de034fc5ad36c86dfcd
liujie <413686520@qq.com>
2017-04-18 00:19:29 +0800

    this is my first commit

--- MyContacts/app/src/main/java/com/liujie/stdy/mycontacts/MainActivity.java
+++ MyContacts/app/src/main/java/com/liujie/stdy/mycontacts/MainActivity.java
@@ -2,12 +2,15 @@ package com.liujie.stdy.mycontacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "LIUJIE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"FIRST COMMIT!");
     }
 }
